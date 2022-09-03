c8ce2467de838e17296b59321f738445a3a561fd
MD Shehab Hossen <shehab.web1@gmail.com>
2022-09-03 13:26:16 +0600

    1
    1

---
+++ precache-manifest.1eea67166ac953db6aa13e6123be28b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f2b096811d80abd1463",
+    "url": "/static/css/main.544a27d8.chunk.css"
+  },
+  {
+    "revision": "1f2b096811d80abd1463",
+    "url": "/static/js/main.1f2b0968.chunk.js"
+  },
+  {
+    "revision": "91d1d4c5a6814aad6413",
+    "url": "/static/js/1.91d1d4c5.chunk.js"
+  },
+  {
+    "revision": "17c3c2b996fe4969b924",
+    "url": "/static/js/2.17c3c2b9.chunk.js"
+  },
+  {
+    "revision": "c1b6e4f0035755e25821",
+    "url": "/static/js/runtime~main.c1b6e4f0.js"
+  },
+  {
+    "revision": "95f238a51a560680815e09e999af5cb9",
+    "url": "/static/media/logo.95f238a5.png"
+  },
+  {
+    "revision": "e800c8469ca70ca84c718283496ee633",
+    "url": "/static/media/giphy.e800c846.gif"
+  },
+  {
+    "revision": "1359b2a1039410d1668c2da1e719df7f",
+    "url": "/index.html"
+  }
+];
